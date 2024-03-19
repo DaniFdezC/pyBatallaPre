317104fee13c954b8f40c0dbfe1e8bd9b2b9f340
Dani <dani96sk@hotmail.com>
2024-03-19 11:21:39 +0100

    Implementación básica de la batalla
    Se agrega la funcionalidad básica de la batalla. Dentro del bucle de la batalla, se comprueba si el héroe acierta o no, dependiendo de los porcentajes realiza una acción o falla. Lo mismo sucede para el vampiro. Si uno de los personajes ataca y derrota al enemigo, se declara ganador y se acaba la batalla. Se añade una funcionalidad para pausar entre cada turno con Scanner

--- src/Battle.java
+++ src/Battle.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Battle {
     public static void main(String[] args) {
         int warriorHealth = 20;
@@ -8,9 +11,46 @@ public class Battle {
         int vampireAttack = 5;
         double vampireSuccessChance = 0.5;
 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
         while (warriorHealth > 0 && vampireHealth > 0){
+
+            double chanceWarriorToAttackThisTurn = random.nextDouble();
+
+            if (chanceWarriorToAttackThisTurn < warriorSuccessChance){
+                vampireHealth -= warriorAttack;
+                System.out.println("El guerrero ataca al vampiro y le inflinge " + warriorAttack + " puntos de daño");
+            } else {
+                System.out.println("El guerrero falla su ataque");
+            }
+
+            if (vampireHealth >= 0){
+                System.out.println("El guerrero ha derrotado al vampiro");
+            } else {
+
+                double chanceVampireToAttackThisTurn = random.nextDouble();
+
+                if (chanceVampireToAttackThisTurn < vampireSuccessChance){
+                    warriorHealth -= vampireAttack;
+                    System.out.println("El vampiro ataca al guerrero y le inflinge " + vampireAttack + " puntos de daño");
+                } else {
+                    System.out.println("El vampiro falla su ataque");
+                }
+
+                if (warriorHealth <= 0){
+                    System.out.println("El vampiro ha derrotado al guerrero");
+                }
+            }
+
+            System.out.println("Vida actual del guerrero: " + warriorHealth);
+            System.out.println("Vida actual del vampiro" + vampireHealth);
+            System.out.println();
+            System.out.print("Presiona enter para continuar...");
+            scanner.nextLine();
 
         }
 
+        scanner.close();
     }
 }
