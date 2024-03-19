13f316647f93482d53cca0c11e7813291c633176
Dani <dani96sk@hotmail.com>
2024-03-19 11:12:27 +0100

    Implementación lógica de la batalla en Battle.java
    Se agrega la estructura básica para la batalla, con variables para cada personaje (vida, ataque, porcentaje de acierto). Además, se crea el bucle en el que sucederán los distintos turnos de los personajes

---
+++ src/Battle.java
@@ -0,0 +1,16 @@
+public class Battle {
+    public static void main(String[] args) {
+        int warriorHealth = 20;
+        int warriorAttack = 2;
+        double warriorSuccessChance = 0.5;
+
+        int vampireHealth = 10;
+        int vampireAttack = 5;
+        double vampireSuccessChance = 0.5;
+
+        while (warriorHealth > 0 && vampireHealth > 0){
+
+        }
+
+    }
+}
