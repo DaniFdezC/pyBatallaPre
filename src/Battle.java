bb653ec4286f3e551bc5e74b2e1c7852c4da56ee
DaniFdezC <dani96sk@hotmail.com>
2024-04-03 11:16:42 +0200

    Reto base Se calculan los porcentajes utilizando Random, el guerrero ataca y tras él, el vampiro

--- src/Battle.java
+++ src/Battle.java
@@ -3,29 +3,24 @@ import java.util.Scanner;
 
 public class Battle {
     public static void main(String[] args) {
-        int[] warriorDamageWeapons = {7, 15, 30};
-        double[] warriorSuccessChanceWeapons = {0.5, 0.25, 0.12};
         int warriorHealth = 20;
+        int warriorAttack = 2;
+        double warriorSuccessChance = 0.5;
 
-
-        int[] vampireDamageWeapons = {5, 10, 20};
-        double[] vampireSuccessChanceWeapons = {0.9, 0.6, 0.4};
         int vampireHealth = 10;
+        int vampireAttack = 5;
+        double vampireSuccessChance = 0.5;
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         while (warriorHealth > 0 && vampireHealth > 0){
 
-            int warriorChosenWeaponIndex = chooseWeapon(warriorDamageWeapons.length, scanner);
-            int warriorChosenWeaponDamage = warriorDamageWeapons[warriorChosenWeaponIndex];
-            double warriorChosenWeaponSuccess = warriorSuccessChanceWeapons[warriorChosenWeaponIndex];
-
             double chanceWarriorToAttackThisTurn = random.nextDouble();
 
-            if (chanceWarriorToAttackThisTurn < warriorChosenWeaponSuccess){
-                vampireHealth -= warriorChosenWeaponDamage;
-                System.out.println("El guerrero ataca al vampiro y le inflinge " + warriorChosenWeaponDamage + " puntos de daño");
+            if (chanceWarriorToAttackThisTurn < warriorSuccessChance){
+                vampireHealth -= warriorAttack;
+                System.out.println("El guerrero ataca al vampiro y le inflinge " + warriorAttack + " puntos de daño");
             } else {
                 System.out.println("El guerrero falla su ataque");
             }
@@ -33,16 +28,11 @@ public class Battle {
             if (vampireHealth <= 0){
                 System.out.println("El guerrero ha derrotado al vampiro");
             } else {
-
-                int vampireChosenWeaponIndex = random.nextInt(3);
-                int vampireChosenWeaponDamage = vampireDamageWeapons[vampireChosenWeaponIndex];
-                double vampireChosenWeaponSuccess = vampireSuccessChanceWeapons[vampireChosenWeaponIndex];
-
                 double chanceVampireToAttackThisTurn = random.nextDouble();
 
-                if (chanceVampireToAttackThisTurn < vampireChosenWeaponSuccess){
-                    warriorHealth -= vampireChosenWeaponDamage;
-                    System.out.println("El vampiro ataca al guerrero y le inflinge " + vampireChosenWeaponDamage + " puntos de daño");
+                if (chanceVampireToAttackThisTurn < vampireSuccessChance){
+                    warriorHealth -= vampireAttack;
+                    System.out.println("El vampiro ataca al guerrero y le inflinge " + vampireAttack + " puntos de daño");
                 } else {
                     System.out.println("El vampiro falla su ataque");
                 }
@@ -55,22 +45,11 @@ public class Battle {
             System.out.println("Vida actual del guerrero: " + warriorHealth);
             System.out.println("Vida actual del vampiro: " + vampireHealth);
             System.out.println();
-            System.out.println("Presiona enter para continuar...");
+            System.out.print("Presiona enter para continuar...");
             scanner.nextLine();
 
         }
 
         scanner.close();
-    }
-
-    public static int chooseWeapon(int maxChoices, Scanner scanner){
-        int chosen;
-
-        do {
-            System.out.println("Elije un arma entre 3 opciones - Arma 1 ( 7 - 50%) ; Arma 2 (15 - 25%) ; Arma 3 (30 - 12%)");
-            chosen = scanner.nextInt();
-        } while (chosen < 1 || chosen > maxChoices);
-
-        return chosen - 1;
     }
 }
